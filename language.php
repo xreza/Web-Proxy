c1654c6ac78366b202f9577e4a04e81376029efc
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-06-28 15:59:02 +0530

    Add files via upload

---
+++ language.php
@@ -0,0 +1,9 @@
+<?php
+$lang["slogan"] = "Removing The Borders, Surf Safe Net.";
+$lang["wikipedia"] = "Wikipedia";
+$lang["back"] = "Back";
+$lang["go"] = "Go";
+$lang["home"] = "Home";
+$lang["tos"] = "https://hashhackers.com/terms";
+$lang["agree"] = "By visiting this page you agree to the";
+$lang["tos_2"] = "Terms and Conditions to use this website.";
